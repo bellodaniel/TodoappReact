e4d9db0b1748881964ece6fc6000e9228c88f5cb
Daniel Bello <daniel@sherocommerce.com>
2018-06-12 13:03:53 +0200

    first commit

---
+++ src/list_elements.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Todolist from './todolist';
+
+class ListElements extends Component {
+    render() {
+        return (
+          <h1>Test From Shero</h1>
+        );
+    }
+}
+
+export default ListElements;
